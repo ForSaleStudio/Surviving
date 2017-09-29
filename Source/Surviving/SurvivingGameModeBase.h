8be28337f9126c14459be909671fd03b4a73e9bc
Caleb Brown <caleb.matthew.brown@gmail.com>
2017-09-28 21:02:34 -0400

    Initial commit with a blank c++ project

---
+++ Source/Surviving/SurvivingGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurvivingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVING_API ASurvivingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
